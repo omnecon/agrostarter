7e83713737be4fccc4b1fa4e1eb74cd610dd2bea
Philipp Potisk <philipp.potisk@bydeluxe.com>
2018-05-02 13:47:11 +0200

    deploy.sh is working now

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,7 @@
+#!/bin/bash
+export AWS_ACCESS_KEY_ID=AKIAJG7PPNFI2XIZ2W7A;
+export AWS_SECRET_ACCESS_KEY=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;
 rm -rf ./dist
 ng build
-aws s3 sync ./dist/ s3://agruno-dev01 --delete 
+aws s3 sync ./dist/ s3://agruno-dev01 --delete
+echo "Deployed updated successfully at: https://dev01.agruno.xyz/"; 
